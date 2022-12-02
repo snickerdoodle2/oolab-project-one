bfbd6e2d0d99b794c59503b46519975e0e44a8ef
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-12-02 23:12:08 +0100

    Test

--- src/main/java/agh/ics/oop/Main.java
+++ src/main/java/agh/ics/oop/Main.java
@@ -4,6 +4,6 @@ import agh.ics.oop.utility.Vector2D;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(new Vector2D(1, 2));
+        System.out.println(new Vector2D(2, 1));
     }
 }
