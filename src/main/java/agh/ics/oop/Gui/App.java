571555d73b3a726f7526fe6eb9f750642039d27a
Dominik Pilipczuk <dominikpil123@gmail.com>
2022-12-19 08:52:15 +0100

    merged

--- src/main/java/agh/ics/oop/Gui/App.java
+++ src/main/java/agh/ics/oop/Gui/App.java
@@ -6,7 +6,6 @@ import agh.ics.oop.Maps.Earth;
 import agh.ics.oop.Maps.WorldMap;
 import agh.ics.oop.Plants.PlantGeneratorsList;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
